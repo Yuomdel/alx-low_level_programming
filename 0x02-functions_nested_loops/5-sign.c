ec804c78bf10f69ba9db543b35759b36da609d88
Yuomdel <garmatimothy0@gmail.com>
2023-09-12 16:46:04 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@
  * Return: 1 is greater than zero. 0 is zero.
  * -1 is less than zero.
  */
-int  print_sign(int n)
+int  print_sign (int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
-return(1);
+		_putchar (43);
+return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar(48);
+		_putchar (48);
 		return (0);
 	}
-	_putchar('\n');
+	_putchar ('\n');
 }
